875a2b4bc8a4727069f9f4b61f4afed04e21a81e
paulameg <paulatakahashi4@gmail.com>
2024-01-31 17:55:33 -0300

    Search Button component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import gitLogo from '../assets/github.png';
 import ItemRepo from '../components/ItemRepo';
 import Input from '../components/input';
+import Button from '../components/Button';
 
 import { Container } from './styles';
 
 function App() {
+
+  const [repos, setRepos] = useState([]);
+
   return (
     <Container>
       <img src={gitLogo} alt="Github logo" width={72} height={72}/>
       <Input />
+      <Button />
       <ItemRepo />
     </Container>
   );
