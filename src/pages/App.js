c199d37b19ab3d06263fd04a50891873db88941f
paulameg <paulatakahashi4@gmail.com>
2024-01-31 18:19:13 -0300

    Search repos function

--- src/pages/App.js
+++ src/pages/App.js
@@ -4,18 +4,39 @@ import ItemRepo from '../components/ItemRepo';
 import Input from '../components/input';
 import Button from '../components/Button';
 
+import { api } from '../services/api';
+
 import { Container } from './styles';
 
 function App() {
 
+  const [currentRepo, setCurrentRepo] = useState('');
   const [repos, setRepos] = useState([]);
+
+  const handleSearchRepo = async () => {
+    const {data} = await api.get(`repos/${currentRepo}`)
+
+    if(data.id) {
+
+      const isExist = repos.find(repo => repo.id === data.id);
+
+      if(!isExist) {
+        setRepos(prev => [...prev, data]);
+        setCurrentRepo('');
+        return
+      }
+      
+    }
+    alert('Repositório não encontrado');
+  }
 
   return (
     <Container>
       <img src={gitLogo} alt="Github logo" width={72} height={72}/>
-      <Input />
-      <Button />
-      <ItemRepo />
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
+      <Button onClick={handleSearchRepo} />
+      {repos.map(repo => <ItemRepo repo={repo} />)}
+      
     </Container>
   );
 }
